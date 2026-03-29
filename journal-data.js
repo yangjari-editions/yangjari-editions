c9d0aa318ba6c167980cbd38c11c1331a5a687d8
yangjari-editions <mail@yangjari-editions.com>
2026-03-29 19:06:01 +0900

    Add files via upload

--- journal-data.js
+++ journal-data.js
@@ -4,7 +4,7 @@
 const JOURNAL_POSTS = [
   {
     href:   "journal/260328_day.html",
-    img:    "images/news/260328_day/text_pg_05.webp",
+    img:    "images/journal/260328_day/text_pg_05.webp",
     title:  "매일매일",
     author: "말립",
     date:   "2026년 3월 28일"
