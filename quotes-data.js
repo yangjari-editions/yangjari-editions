ce0ea1843d39d1eeab74ace23b5c7e27f6ecbc3f
yangjari-editions <mail@yangjari-editions.com>
2026-04-04 12:06:50 +0900

    Add files via upload

--- quotes-data.js
+++ quotes-data.js
@@ -4,5 +4,5 @@
 
 const QUOTES = [
   { line: "내 영혼은 지치고, 허약하고, 슬프다.", source: "— 조리스카를 위스망스(Joris-Karl Huysmans, 1848–1907)" },
-  { line: "저는 누군가를 위해 글쓰는 게 아닙니다. 사물 자체를 위해 씁니다.", source: "— 루이페르디낭 셀린(Louis-Ferdinand Céline, 1894-1961)" }
+  { line: "저는 누군가를 위해 글쓰는 것이 아닙니다. 사물 자체를 위해 씁니다.", source: "— 루이페르디낭 셀린(Louis-Ferdinand Céline, 1894–1961)" }
 ];
