4d54fcc3c98f2ab63625f74e99eec74afe09247d
yangjari-editions <mail@yangjari-editions.com>
2026-04-11 09:38:42 +0900

    Add files via upload

--- journal-data.js
+++ journal-data.js
@@ -5,7 +5,7 @@ const JOURNAL_POSTS = [
   {
     href:   "/journal/day_01/",
     img:    "images/news/day_01/text_pg_05.webp",
-    title:  "이옥준: 하루",
+    title:  "침대에서 읽은 책",
     author: "",
     date:   "2026년 3월 28일"
   }
