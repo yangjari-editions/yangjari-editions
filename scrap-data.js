791ac4a86e1a473cd0a6bf662ca3d0611868eeac
yangjari-editions <mail@yangjari-editions.com>
2026-04-25 08:14:58 +0900

    Add files via upload

--- scrap-data.js
+++ scrap-data.js
@@ -7,8 +7,8 @@ const SCRAP_ITEMS = [
   { img: "images/scrap/journal.jpg", alt: "비톨드 곰브로비치, 일기, 2권, 갈리마르, 1995", title: "비톨드 곰브로비치, 일기, 2권, 갈리마르, 1995", width: "25%", gap: "20" },
   { img: "images/scrap/9791141601560.jpg", alt: "앙리 바르뷔스, 포화, 문학동네, 2024", title: "앙리 바르뷔스, 포화, 문학동네, 2024", width: "25%", gap: "20" },
   { img: "images/scrap/51m-ZRweCmL.jpg", alt: "장 에슈노즈, 제롬 랭동, 미뉘, 2001", title: "장 에슈노즈, 제롬 랭동, 미뉘, 2001", width: "40%", gap: "20" },
-  { img: "images/scrap/couv_pacadis_jeune_homme.jpg", alt: "세련된 젊은이, 알랭 파카디, 에로-리미트, 2018", title: "세련된 젊은이, 알랭 파카디, 에로-리미트, 2018", width: "40%", gap: "20" },
-  { img: "images/scrap/9788932020440.jpg", alt: "어린시절, 나탈리 사로트, 문학과 지성사, 2010", title: "어린시절, 나탈리 사로트, 문학과 지성사, 2010", width: "25%", gap: "20" },
-  { img: "images/scrap/9782080206268_1_75.jpg", alt: "시집, 프랑수아 비용, 플라마리옹, 2020", title: "시집, 프랑수아 비용, 플라마리옹, 2020", width: "20%", gap: "20" },
-  { img: "images/scrap/XL.jpeg", alt: "팡타그뤼엘 제3서, 프랑수아 라블레, 한길사, 2006", title: "팡타그뤼엘 제3서, 프랑수아 라블레, 한길사, 2006", width: "33%", gap: "20" }
+  { img: "images/scrap/couv_pacadis_jeune_homme.jpg", alt: "알랭 파카디, 세련된 젊은이, 에로-리미트, 2018", title: "알랭 파카디, 세련된 젊은이, 에로-리미트, 2018", width: "40%", gap: "20" },
+  { img: "images/scrap/9788932020440.jpg", alt: "나탈리 사로트, 어린시절, 문학과 지성사, 2010", title: "나탈리 사로트, 어린시절, 문학과 지성사, 2010", width: "25%", gap: "20" },
+  { img: "images/scrap/9782080206268_1_75.jpg", alt: "프랑수아 비용, 시집, 플라마리옹, 2020", title: "프랑수아 비용, 시집, 플라마리옹, 2020", width: "20%", gap: "20" },
+  { img: "images/scrap/XL.jpeg", alt: "프랑수아 라블레, 팡타그뤼엘 제3서, 한길사, 2006", title: "프랑수아 라블레, 팡타그뤼엘 제3서, 한길사, 2006", width: "33%", gap: "20" }
 ];
