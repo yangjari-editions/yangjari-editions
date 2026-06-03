e06cff6fb1549903edb182b83ded99bedfae5df3
yangjari-editions <mail@yangjari-editions.com>
2026-06-03 10:24:00 +0900

    Add files via upload

--- text-data.js
+++ text-data.js
@@ -17,7 +17,7 @@ const TEXT_POSTS = [
     author:     "에밀 시오랑",
     translator: "표은선 번역",
     date:       "2026년 6월 3일",
-    desc:       "「깨우기 위해 쓰다(Escribir para despertar)」, 『엘 파이스 El País』, 1977년"
+    desc:       "「깨우기 위해 쓰다」, 『엘 파이스(El País)』, 1977년"
   },
   {
     href:       "/text/celine_rabelais/",
